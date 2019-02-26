19b57f385331c226e6b22bd6e0aa9ee3eccb8799
cmarisco <45523456+cmarisco@users.noreply.github.com>
2019-02-26 10:26:18 -0800

    Add files via upload

---
+++ Morse/Morse.cpp
@@ -0,0 +1,29 @@
+/*
+ * Christopher Marisco
+ * Source file for library "Morse".
+ * 2/23/19
+ */ 
+
+#include "Arduino.h"
+#include "Morse.h"
+
+/*****Constructor*****/
+
+Morse::Morse(int pin){
+    pinMode(pin, OUTPUT);
+    _pin = pin;
+}
+
+void Morse::dot(){
+    digitalWrite(_pin, HIGH);
+    delay(250);
+    digitalWrite(_pin, LOW);
+    delay(250);
+}
+
+void Morse::dash(){
+    digitalWrite(_pin, HIGH);
+    delay(1000);
+    digitalWrite(_pin, HIGH);
+    delay(250);
+}
